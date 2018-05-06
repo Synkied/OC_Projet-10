6ca7bef19aff037d68062924f909a83529c3e812
Synkied <synkx@hotmail.fr>
2018-05-07 00:00:12 +0200

    dask introduction

--- nutellove/csv_cleaner.py
+++ nutellove/csv_cleaner.py
@@ -1,6 +1,9 @@
 # coding: utf8
 
+import time
+
 import pandas as pd
+import dask.dataframe as dd
 
 from constants import *
 
@@ -23,16 +26,36 @@ class CSVCleaner():
         fname = self.file_name
 
         print("Cleaning CSV file... Please wait...")
+        start = time.time()
+
+        dtypes = {
+            'code': 'object',
+            'created_t': 'object',
+            'last_modified_t': 'object',
+            'cities': 'object',
+            'allergens_fr': 'object',
+            'cities_tags': 'object',
+            'emb_codes': 'object',
+            'emb_codes_tags': 'object',
+            'first_packaging_code_geo': 'object',
+            'generic_name': 'object',
+            'ingredients_from_palm_oil_tags': 'object',
+            'origins': 'object',
+            'origins_tags': 'object',
+            'stores': 'object'
+        }
 
         # reads the specified file.
         # sep: csv file's separator
         # low_memory: avoiding unnecessary warning msgs
-        csv_file = pd.read_csv(
+        csv_file = dd.read_csv(
             fname,
             sep="\t",
             encoding="utf-8",
-            low_memory=False,
             thousands=',',
+            low_memory=False,
+            dtype=dtypes,
+            blocksize=25e6,
         )
 
         # defines a dataframe, from the passed headers
@@ -52,12 +75,18 @@ class CSVCleaner():
         new_f['nutrition_grade_fr'] = new_f['nutrition_grade_fr'].str.lower()
 
         # save the new file to a csv file, with the name "db_file.csv"
-        new_f.to_csv(
-            "db_file.csv",
+        new_f.compute().to_csv(
+            "db_file/db_file.csv",
             index=False,
             encoding="utf-8",
             sep=";",
         )
+
+        end = time.time()
+
+        duration = (end - start)
+
+        print(duration, "elapsed.")
 
 
 if __name__ == "__main__":
