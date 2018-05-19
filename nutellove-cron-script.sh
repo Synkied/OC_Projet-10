6f1ca79eb4e5db3d7ab3f0f23fb97dd2b852653d
Quentin <synkx@hotmail.fr>
2018-05-19 17:25:19 +0200

    nutellove-cron-script

--- nutellove-cron-script.sh
+++ nutellove-cron-script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo /usr/bin/wget https://fr.openfoodfacts.org/data/fr.openfoodfacts.org.products.csv -O /home/quentin/nutellove_project/fr.openfoodfacts.org.products.csv ; /home/quentin/virtualenvs/nutellove/bin/python3 /home/quentin/nutellove_project/csv_cleaner.py ; /home/quentin/virtualenvs/nutellove/bin/python3 /home/quentin/nutellove_project/db_feeding.py
+sudo /usr/bin/wget https://fr.openfoodfacts.org/data/fr.openfoodfacts.org.products.csv -O /media/pi/my_passport/nutellove_project/fr.openfoodfacts.org.products.csv && cd /media/pi/my_passport/nutellove_project && /home/pi/berryconda3/envs/nutellove/bin/python3 /media/pi/my_passport/nutellove_project/csv_cleaner.py && cd /media/pi/my_passport/nutellove_project && /home/pi/berryconda3/envs/nutellove/bin/python3 /media/pi/my_passport/nutellove_project/db_feeding.py
